b80f9da11c3233c52de13106838860f62212fabf
Kirill <kirill1098@gmail.com>
2019-04-21 17:08:38 +0300

    Add Firebase reading and items structure

--- app/src/main/java/com/edwardvanraak/materialbarcodescannerexample/MainActivity.java
+++ app/src/main/java/com/edwardvanraak/materialbarcodescannerexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.edwardvanraak.materialbarcodescannerexample;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,35 +9,57 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.edwardvanraak.materialbarcodescanner.MaterialBarcodeScanner;
 import com.edwardvanraak.materialbarcodescanner.MaterialBarcodeScannerBuilder;
+import com.edwardvanraak.materialbarcodescannerexample.madels.Product;
+import com.edwardvanraak.materialbarcodescannerexample.madels.RecyclerView_Config;
 import com.google.android.gms.vision.barcode.Barcode;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.Context;
 
+
+import java.util.List;
 
 import static junit.framework.Assert.assertNotNull;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+
     public static final String BARCODE_KEY = "BARCODE";
 
     private Barcode barcodeResult;
+//    private TextView result;
 
-    private TextView result;
+    private static final String TAG = "PostDetailActivity";
+
+
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_products);
+
+        workFirebase();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        result = (TextView) findViewById(R.id.barcodeResult);
+
+//        result = (TextView) findViewById(R.id.barcodeResult);
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assertNotNull(result);
+//        assertNotNull(result);
         assertNotNull(fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,20 +70,64 @@ public class MainActivity extends AppCompatActivity {
         if(savedInstanceState != null){
             Barcode restoredBarcode = savedInstanceState.getParcelable(BARCODE_KEY);
             if(restoredBarcode != null){
-                result.setText(restoredBarcode.rawValue);
+//                result.setText(restoredBarcode.rawValue);
                 barcodeResult = restoredBarcode;
             }
         }
     }
 
+    private void workFirebase() {
+        new FirebaseDatabaseHelper().readProducts(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Product> products, List<String> keys) {
+                new RecyclerView_Config().setConfig(mRecyclerView,MainActivity.this, products, keys);
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdeted() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
+    }
+
     private void showCode(String txt) {
+//        myRef = FirebaseDatabase.getInstance().getReference();
+//
+//        myRef.child("users_item").child("product").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                String value = dataSnapshot.getValue(String.class);
+//                result.setText(value + "112");
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                result.setText("errrrrrrrrrrroor");
+//                // Failed to read value
+//
+//            }
+//        });
 
         Intent intent = new Intent(MainActivity.this, ProductAct.class);
 
         intent.putExtra("barcode",txt);
         startActivity(intent);
-
     }
+
+
+
 
     private void startScan() {
 
@@ -75,7 +142,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResult(Barcode barcode) {
                         barcodeResult = barcode;
-                        result.setText(barcode.rawValue); // убрать с началього экрана
                         String txt = barcode.rawValue;
                         showCode(txt);
                     }
@@ -111,4 +177,6 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton(android.R.string.ok, listener)
                 .show();
     }
+
+
 }
--- app/src/main/java/com/edwardvanraak/materialbarcodescannerexample/ProductAct.java
+++ app/src/main/java/com/edwardvanraak/materialbarcodescannerexample/ProductAct.java
@@ -14,20 +14,27 @@ public class ProductAct extends AppCompatActivity {
     private DatabaseReference myRef;
     private FirebaseDatabase database;
 
+    TextView txt_result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_product);
+        txt_result = (TextView) findViewById(R.id.info);
+
         String datatxt = getIntent().getStringExtra("barcode");
         postRequest(datatxt);
     }
 
     private void postRequest(String datatxt) {
         database = FirebaseDatabase.getInstance();
-        database.setPersistenceEnabled(true);
+//        database.setPersistenceEnabled(true);
         myRef = database.getReference("users_item").child("product");
         myRef.push().setValue(datatxt);
+
+        txt_result.setText(datatxt);
+
     }
 
 }
